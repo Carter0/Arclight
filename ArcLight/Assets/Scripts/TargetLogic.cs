49e0790c56ab8cf52e8ae4d30a692cf334a1fc1b
carter <weinbergcarter@gmail.com>
2020-03-10 15:50:42 -0400

    Adding target controller script

---
+++ ArcLight/Assets/Scripts/TargetLogic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TargetLogic : MonoBehaviour
+{
+    public GameObject targetDoor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            //targetDoor.Open();
+        }
+    }
+}
